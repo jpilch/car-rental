64cbe00bde86be9966651706d4cf95e1d389e26c
jpilch <249546@student.pwr.edu.pl>
2022-01-08 21:50:15 +0100

    create pydantic car schemas

---
+++ backend/schemas.py
@@ -0,0 +1,52 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class CarBase(BaseModel):
+    pass
+
+
+class CarCreate(CarBase):
+    pass
+
+
+class Car(CarBase):
+    id: int
+    model_id: int
+    is_available: bool
+
+    class Config:
+        orm_mode = True
+
+
+class ModelBase(BaseModel):
+    name: str
+
+
+class ModelCreate(ModelBase):
+    pass
+
+
+class Model(ModelBase):
+    id: int
+    manufacturer_id: int
+    cars = List[Car]
+
+    class Config:
+        orm_mode = True
+
+
+class ManufacturerBase(BaseModel):
+    name: str
+
+
+class ManufacturerCreate(ManufacturerBase):
+    pass
+
+
+class Manufacturer(ManufacturerBase):
+    id: int
+    models: List[Model] = []
+
+    class Config:
+        orm_mode = True
