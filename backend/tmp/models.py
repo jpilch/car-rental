e760ff60f0853c7718ef49cd7f575c928309d283
jpilch <249546@student.pwr.edu.pl>
2022-01-08 21:16:45 +0100

    store files for later use

--- backend/tmp/models.py
+++ backend/tmp/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
-from .database import Base
+from ..database import Base
 
 
 class User(Base):
@@ -9,18 +9,19 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     role_id = Column(Integer, ForeignKey("role.id"))
-    username = Column(String(16), unique=True)
+    first_name = Column(String(32))
+    last_name = Column(String(32))
     email = Column(String(32), unique=True, index=True)
     password = Column(String(128))
     is_active = Column(Boolean, default=True)
 
-    role = relationship("Role", back_populates="users")
+    user_role = relationship("UserRole", back_populates="users")
 
 
-class Role(Base):
-    __tablename__ = "role"
+class UserRole(Base):
+    __tablename__ = "user_role"
 
     name = Column(String(16))
     description = Column(String(32))
 
-    users = relationship("User", back_populates="role")
+    users = relationship("User", back_populates="user_role")
