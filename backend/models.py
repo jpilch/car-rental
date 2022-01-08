dea586f67569ff9f8dddfbf34a18219ba571008d
jpilch <249546@student.pwr.edu.pl>
2022-01-08 21:33:37 +0100

    create car models

---
+++ backend/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Manufacturer(Base):
+    __tablename__ = "manufacturer"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(32))
+    
+    models = relationship("Model", back_populates="manufacturer")
+
+
+class Model(Base):
+    __tablename__ = "model"
+
+    id = Column(Integer, primary_key=True, index=True)
+    manufacurer_id = Column(Integer, ForeignKey("manufaturer.id"))
+    name = Column(String(32))
+    
+    manufacturer = relationship("Manufacturer", back_populates="models")
+    cars = relationship("Car", back_populates="model")
+
+
+class Car(Base):
+    __tablename__ = "Car"
+
+    id = Column(Integer, primary_key=True, index=True)
+    model_id = Column(Integer, ForeignKey("model.id"))
+    is_avaiable = Column(Boolean, default=True)
+    
+    model = relationship("Model", back_populates="cars")
