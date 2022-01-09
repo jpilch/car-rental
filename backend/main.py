67a70a95766d4877ead22f91f09d47841bc9c7d9
jpilch <249546@student.pwr.edu.pl>
2022-01-09 12:41:14 +0100

    refactor post and add delete endpoint for manufacturer model

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 import uvicorn 
 
-import models, schemas
+import models, schemas, crud
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -25,15 +25,19 @@ async def root():
 
 @app.post("/manufacturers/", response_model=schemas.Manufacturer)
 async def create_manufacturer(manufacturer: schemas.ManufacturerCreate, db: Session = Depends(get_db)):
-    db_manufacturer = db.query(models.Manufacturer).filter(models.Manufacturer.name == manufacturer.name).first()
+    db_manufacturer = crud.get_manufacturer(db, manufacturer)
     if db_manufacturer:
         raise HTTPException(status_code=400, detail="Manufacturer already exists")
-    db_manufacturer = models.Manufacturer(name=manufacturer.name)
-    db.add(db_manufacturer)
-    db.commit()
-    db.refresh(db_manufacturer)
-    return db_manufacturer
+    return crud.create_manufacturer(db=db, manufacturer=manufacturer)
 
+
+@app.delete("/manufacturers/{manufacturer_id}")
+def delete_manufacturer(manufacturer_id: int, db: Session = Depends(get_db)):
+    db_manufacturer = crud.get_manufacturer(db, manufacturer_id)
+    if not db_manufacturer:
+        raise HTTPException(status_code=404, detail="Manufacturer does not exist")
+    return crud.delete_manufacturer(db, db_manufacturer)
+    
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
