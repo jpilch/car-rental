01ad471d2aeca8a017671a5edcfe672c4d7511f0
jpilch <249546@student.pwr.edu.pl>
2022-01-14 14:33:21 +0100

    include aut router in main app

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import uvicorn
 
 import models
 from database import engine
-from routers import car, manufacturer, model
+from routers import car, manufacturer, model, auth
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -11,6 +11,7 @@ app = FastAPI()
 app.include_router(manufacturer.router)
 app.include_router(model.router)
 app.include_router(car.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
