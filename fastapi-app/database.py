615ab075377764fa2398d9ec464edf66f2647ccf
jpilch <249546@student.pwr.edu.pl>
2022-03-03 17:29:38 +0100

    add docker compose file with postgres & pgadmin services

--- fastapi-app/database.py
+++ fastapi-app/database.py
@@ -3,10 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
+POSTGRESQL_DATABASE_URL = "postgresql+psycopg2://fastapi:secret@localhost:5432/car_rental"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+#engine = create_engine(
+#    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+#)
+
+engine = create_engine(POSTGRESQL_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
