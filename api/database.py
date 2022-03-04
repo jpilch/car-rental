3dc1bd7cdcda12daf6adeb2cd5320d1fd01ca0b0
jpilch <249546@student.pwr.edu.pl>
2022-03-04 09:29:01 +0100

    remove sensitive variables

--- api/database.py
+++ api/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-POSTGRESQL_DATABASE_URL = "postgresql+psycopg2://fastapi:secret@db:5432/car_rental"
+import os
+url = os.environ.get('POSTGRESQL_DATABASE_URL')
 
-engine = create_engine(POSTGRESQL_DATABASE_URL)
+engine = create_engine(url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
