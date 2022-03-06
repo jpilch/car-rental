eb2eaa327a8d5835880c8da831e113489ce47db5
jpilch <249546@student.pwr.edu.pl>
2022-03-06 13:31:12 +0100

    add cors middleware to api to enable cross domain calls

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 import models
@@ -7,6 +8,21 @@ from routers import car, location, city, manufacturer, model, auth, rental, agre
 
 # models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(manufacturer.router)
 app.include_router(model.router)
