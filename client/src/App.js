59396fa31bab720f19f254e5141ca57e3822ea15
jpilch <249546@student.pwr.edu.pl>
2022-03-06 12:43:46 +0100

    render navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Landing from './pages/Landing';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing />}/>
         </Routes>
+        <Navbar />
       </Router>
     </>
   );
