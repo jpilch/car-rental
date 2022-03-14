5ea58de3f8d2e47ad6eb0b4f901e05d99fd2c4f7
jpilch <249546@student.pwr.edu.pl>
2022-03-14 16:50:19 +0100

    fix icon positioning on car list view

--- client/src/components/CarItem.jsx
+++ client/src/components/CarItem.jsx
@@ -4,7 +4,7 @@ import {GiCarWheel} from 'react-icons/gi'
 import {MdFamilyRestroom, MdLuggage} from 'react-icons/md'
 import {IoMdResize} from 'react-icons/io'
 import {AiFillStar} from 'react-icons/ai'
-
+import {FaMoneyBillWaveAlt} from 'react-icons/fa'
 function CarItem({id, name, manufacturerName, img_url, drive_cat, person_capacity, trunk_capacity, height, length, width, avg_fuel_consumption}) {
   return (
     <div className='car-item'>
@@ -13,14 +13,49 @@ function CarItem({id, name, manufacturerName, img_url, drive_cat, person_capacit
                 <h2 className="model-name">{manufacturerName} {name}</h2>
                 <div className="specs">
                     <div className="row">
-                        <GiCarWheel /> {drive_cat}
-                        <MdFamilyRestroom /> {person_capacity}
-                        <MdLuggage /> {trunk_capacity}L
-                        <FaBurn /> {avg_fuel_consumption}L/100km
+                        <div className="spec-item">
+                            <div className="icon">
+                                <GiCarWheel />
+                            </div>
+                             {drive_cat}
+                        </div>
+                        <div className="spec-item">
+                            <div className="icon">
+                                <MdFamilyRestroom />
+                            </div>
+                             {person_capacity}
+                        </div>
+                        <div className="spec-item">
+                            <div className="icon">
+                                <MdLuggage />
+                            </div>
+                            {trunk_capacity}L
+                        </div>
+                        <div className="spec-item">
+                            <div className="icon">
+                                <FaBurn />
+                            </div>
+                             {avg_fuel_consumption}L/100km
+                        </div>
                     </div>
                     <div className="row">
-                        <IoMdResize /> {width}(W) x {length}(L) x {height}(H)
-                        <AiFillStar /> 4.5/5
+                        <div className="spec-item">
+                            <div className="icon">
+                                <IoMdResize />
+                            </div> {width}(W) x {length}(L) x {height}(H)
+                        </div>
+                        <div className="spec-item">
+                            <div className="icon">
+                                <AiFillStar />
+                            </div>
+                             4.5/5
+                        </div>
+                        <div className="spec-item">
+                            <div className="icon">
+                                <FaMoneyBillWaveAlt />
+                            </div>
+                            See pricing
+                        </div>
                     </div>
                 </div>
                 <div className="actions">
