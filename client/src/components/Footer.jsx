5396cbf33ad46a5787dae411bf130509fca75dda
jpilch <249546@student.pwr.edu.pl>
2022-03-25 16:37:29 +0100

    rename class property to className

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <footer>
-        <div class="content">
-            <ul>
-                <li><i class="gg-instagram"></i></li>
-                <li><i class="gg-twitter"></i></li>
-                <li><i class="gg-facebook"></i></li>
-                <li><i class="gg-youtube"></i></li>
-            </ul>
-            <p id="copyright">&copy; Copyright 2022 J. Pilch</p>
-        </div>
-    </footer>
-  )
+	return (
+		<footer>
+			<div className="content">
+				<ul>
+					<li><i className="gg-instagram"></i></li>
+					<li><i className="gg-twitter"></i></li>
+					<li><i className="gg-facebook"></i></li>
+					<li><i className="gg-youtube"></i></li>
+				</ul>
+				<p id="copyright">&copy; Copyright 2022 J. Pilch</p>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
