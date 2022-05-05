2471fc6055f4fdd62e0c68d39cc67db0f4753cab
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-05 08:22:08 +0200

    change landing page, with search bar and animation

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,52 +1,18 @@
 import React from 'react'
+import '../css/Landing.css'
 
 export default function Landing() {
 	return (
-		<>
-			<div className="hero">
-				<div className="content">
-					<div className="service-info">
-						<h1>Why choose Us?</h1>
-						<p>We offer wide range of cars suited for your needs.</p>
-						<p>All available just few clicks away from you</p>
-						<button className="btn"><a href="#instruction">Read More</a></button>
-					</div>
+			<main className={'landing'}>
+				<h1>We offer <span className={'number-highlight'}>400</span> cars from <span className={'number-highlight'}>8</span> manufacturers, in <span className={'number-highlight'}>20</span> different Cities!</h1>
+				<div className={'search-wrapper'}>
+					<form>
+						<input type="text" name="search" id="search" placeholder={'Search for your City'}/>
+					</form>
 				</div>
-			</div>
-
-			<div className="instruction" id='instruction'>
-				<div className="content">
-					<h1 className="section-heading">How to do it?</h1>
-					<div className="row-1 row">
-						<div className="tile tile-1">
-							<h1>1. Register</h1>
-							<hr/>
-							<h4>Create an account and fill in the required personal information. Don't worry, we make
-								sure it's
-								safe.
-							</h4>
-
-						</div>
-						<div className="tile tile-2">
-							<h1>2. Choose a car</h1>
-							<hr/>
-							<h4>Make a choice that suits your needs. Choose the pickup as well as return date and
-								location.
-							</h4>
-						</div>
-					</div>
-					<div className="row-2 row">
-						<div className="tile tile-3">
-							<h1>3. Enjoy!</h1>
-							<hr/>
-							<h4>Feel free to use the car as much as you want, as long as you take care of it. No limits
-								or hidden
-								fees.
-							</h4>
-						</div>
-					</div>
+				<div className="service-info">
+					<p>Make the best choice with Us! <span>Read More</span></p>
 				</div>
-			</div>
-		</>
+			</main>
 	)
 }
