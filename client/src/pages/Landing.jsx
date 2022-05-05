d3df78510c3a37338a49707e2eeec765bffc907b
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-05 11:05:04 +0200

    fix landing page layout

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import '../css/Landing.css'
+import useWindowDimensions from "../hooks";
 
 export default function Landing() {
+
 	return (
 			<main className={'landing'}>
-				<h1>We offer <span className={'number-highlight'}>400</span> cars from <span className={'number-highlight'}>8</span> manufacturers, in <span className={'number-highlight'}>20</span> different Cities!</h1>
-				<div className={'search-wrapper'}>
-					<form>
-						<input type="text" name="search" id="search" placeholder={'Search for your City'}/>
-					</form>
+				<div className={'heading'}>
+					<p><span className={'number-highlight'}>400</span> Cars</p>
+					<p><span className={'number-highlight'}>8</span> Manufacturers</p>
+					<p><span className={'number-highlight'}>20</span> Cities</p>
 				</div>
-				<div className="service-info">
-					<p>Make the best choice with Us! <span>Read More</span></p>
+				<div className={'item'}>
+					<div className='search-wrapper'>
+						<form>
+							<input type="text" name="search" id="search" placeholder={'Search for your City'}/>
+						</form>
+					</div>
+					<div className="service-info">
+						<p>Make the best choice with Us! <span>Read More</span></p>
+					</div>
 				</div>
 			</main>
 	)
