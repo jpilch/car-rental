a2a1716237a17f92be97cc1b48e2dad9b1e6b3b8
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-08 09:49:45 +0200

    add async redux action for loggin in the use

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,22 +1,41 @@
 import Button from "./Button";
 import '../css/Form.css'
+import {useDispatch} from "react-redux";
+import {login} from "../reducers/authSlice";
 
 const LoginForm = () => {
 
+	const dispatch = useDispatch()
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		const email = e.target.email.value
+		const password = e.target.password.value
+		e.target.email.value = ''
+		e.target.password.value = ''
+		dispatch(login({email, password}))
+	}
+
 	return (
 		<div className="form-container">
-			<form>
+			<form onSubmit={e => handleSubmit(e)}>
 				<h1>Login Form</h1>
 				<div>
 					<label htmlFor="email">
 						Email
 					</label>
-					<input type="email" id="email"
+					<input
+						type="email"
+						id="email"
+						name={'email'}
 					/>
 				</div>
 				<div>
 					<label htmlFor="password">Password</label>
-					<input type="password" id="password"
+					<input
+						type="password"
+						id="password"
+						name={'password'}
 					/>
 				</div>
 				<Button text={'Submit'} />
--- client/src/reducers/authSlice.js
+++ client/src/reducers/authSlice.js
@@ -1,17 +1,37 @@
 import {createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {}
+const initialState = {
+    user: null
+}
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        log: state => {
-            console.log(state)
+        setUser: (state, action) => {
+            state.user = action.payload
         }
     }
 })
 
-export const {log} = authSlice.actions
+export const {setUser} = authSlice.actions
+
+export const login = ({email, password}) => {
+    return async dispatch => {
+        const data = new FormData()
+        data.append('username', email)
+        data.append('password', password)
+        try {
+            const response = await axios.post(
+                `${process.env.REACT_APP_API_URL}/token`,
+                data
+            )
+            dispatch(setUser(response.data))
+        } catch (e) {
+            console.log('error', e)
+        }
+    }
+}
 
 export default authSlice.reducer
