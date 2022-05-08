011f619c5a57484f8cf12c4a62419a5c5d10492a
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-08 10:49:01 +0200

    register notification reducer in store

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "./reducers/authSlice";
+import notificationReducer from "./reducers/notificationSlice";
 
 export const store = configureStore({
     reducer: {
-        authReducer
+        authReducer,
+        notificationReducer
     }
 })
