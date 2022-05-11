32d1f4408a4226384c0460f170b434a97f4eade9
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-11 11:46:12 +0200

    add 2 endpoints for car model document

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {carModels} = require('./_data')
 
 const app = express()
 
@@ -6,8 +7,22 @@ app.get('/', (req, res) => {
     res.send('Hello from MotoRent API!').end()
 })
 
+app.get('/api/car-models', (req, res) => {
+    console.log(carModels, typeof carModels)
+    res.json(carModels)
+})
+
+app.get('/api/car-models/:id', (req, res) => {
+    const id = +req.params.id
+    const carModel = carModels.find(cm => cm.id === id)
+    if (!carModel) {
+        return res.status(404).end()
+    }
+    res.status(200).json(carModel)
+})
+
 app.get('/api/cars', (req, res) => {
-    res.json({'test': 'test'})
+    res.json({'msg': 'car instances here'})
 })
 
 const PORT = 3001
