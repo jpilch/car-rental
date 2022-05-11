d6ac12061310d0a7aadc789e3d12a97cfa4d47be
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-11 15:15:11 +0200

    add delete and post endpoints for carmodel

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const {carModels} = require('./_data')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello from MotoRent API!').end()
 })
@@ -19,6 +21,22 @@ app.get('/api/car-models/:id', (req, res) => {
         return res.status(404).end()
     }
     res.status(200).json(carModel)
+})
+
+app.delete('/api/car-models/:id', (req, res) => {
+    const id = +req.params.id
+    const carModel = carModels.find(cm => cm.id === id)
+    if (!carModel) {
+        return res.status(404).end()
+    }
+    carModels.filter(cm => cm.id !== id)
+    res.status(201).end()
+})
+
+app.post('/api/car-models', (req, res) => {
+    const carModel = req.body
+    carModels.concat(carModel)
+    res.json(carModel)
 })
 
 app.get('/api/cars', (req, res) => {
