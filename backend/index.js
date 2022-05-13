fdbc18c3e1783644e9347f84e4ddaa71c5bffb26
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-13 09:23:46 +0200

    create app server

--- backend/index.js
+++ backend/index.js
@@ -1,68 +1,10 @@
-const express = require('express')
-const {
-    morganLogger,
-    errorHandler,
-    unknownEndpoint
-} = require('./middleware')
+const app = require('app')
+const http = require('http')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 
-const CarModel = require('./models/carmodel')
+const server = http.createServer(app)
 
-const app = express()
-
-app.use(express.json())
-app.use(morganLogger)
-
-app.get('/', (req, res) => {
-    res.send('Hello from MotoRent API!').end()
-})
-
-app.get('/api/car-models', async (req, res) => {
-    const carModels = await CarModel.find({})
-    res.json(carModels)
-})
-
-app.get('/api/car-models/:id', (req, res, next) => {
-    CarModel.findById(req.params.id)
-        .then(carModel => {
-            if (carModel) {
-                res.json(carModel)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(err => next(err))
-})
-
-app.post('/api/car-models', (req, res, next) => {
-    const carModel = new CarModel(req.body)
-    carModel.save()
-        .then(savedCarModel => {
-            res.json(savedCarModel)
-        })
-        .catch(err => next(err))
-})
-
-app.put('/api/car-models/:id', (req, res, next) => {
-    CarModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        .then(updatedCarModel => res.json(updatedCarModel))
-        .catch(err => next(err))
-})
-
-app.delete('/app/car-models/:id', (req, res, err, next) => {
-    CarModel.findByIdAndRemove(req.params.id)
-        .then(() => {
-            res.status(204).end()
-        })
-        .catch(err => next(err))
-})
-
-app.get('/api/cars', (req, res) => {
-    res.json({ msg: 'car instances here' })
-})
-
-app.use(errorHandler)
-app.use(unknownEndpoint)
-
-app.listen(process.env.PORT || 3001, () => {
-    console.log(`Server running on port ${process.env.PORT || 3001}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server listening on port ${config.PORT}`)
 })
