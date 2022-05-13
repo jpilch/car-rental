5e2e010593f9c13180576ed5ba428669518edcf7
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-13 09:45:53 +0200

    create car schema

---
+++ backend/models/car.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const carSchema = new mongoose.Schema({
+    isAvailable: {
+        type: Boolean,
+        required: true
+    },
+    carModel: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CarModel',
+        required: true
+    }
+})
+
+carSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Car', carSchema)
