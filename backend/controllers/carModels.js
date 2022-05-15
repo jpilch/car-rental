be775620053f6d42bad3dea47ad23830fa025efd
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-15 09:25:13 +0200

    rewrite carmodel controller using async/await syntax

--- backend/controllers/carModels.js
+++ backend/controllers/carModels.js
@@ -6,39 +6,29 @@ carModelsRouter.get('/', async (req, res) => {
     res.json(carModels)
 })
 
-carModelsRouter.get('/:id', (req, res, next) => {
-    CarModel.findById(req.params.id)
-        .then(carModel => {
-            if (carModel) {
-                res.json(carModel)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(err => next(err))
+carModelsRouter.get('/:id', async (req, res, next) => {
+    const carModel = await CarModel.findById(req.params.id)
+    if (carModel) {
+        res.json(carModel)
+    } else {
+        res.status(404).end()
+    }
 })
 
-carModelsRouter.post('/', (req, res, next) => {
+carModelsRouter.post('/', async (req, res, next) => {
     const carModel = new CarModel(req.body)
-    carModel.save()
-        .then(savedCarModel => {
-            res.json(savedCarModel)
-        })
-        .catch(err => next(err))
+    const saveCarModel = await carModel.save()
+    res.json(saveCarModel)
 })
 
-carModelsRouter.put('/:id', (req, res, next) => {
-    CarModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        .then(updatedCarModel => res.json(updatedCarModel))
-        .catch(err => next(err))
+carModelsRouter.put('/:id', async (req, res, next) => {
+    const updatedCarModel = await CarModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.json(updatedCarModel)
 })
 
-carModelsRouter.delete('/carModelsRouter/car-models/:id', (req, res, err, next) => {
-    CarModel.findByIdAndRemove(req.params.id)
-        .then(() => {
-            res.status(204).end()
-        })
-        .catch(err => next(err))
+carModelsRouter.delete('/carModelsRouter/car-models/:id', async (req, res, err, next) => {
+    await CarModel.findByIdAndRemove(req.params.id)
+    res.status(204).end()
 })
 
 module.exports = carModelsRouter
