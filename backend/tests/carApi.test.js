0e5357667e671fa46be1a98556f90d3228682f1c
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-15 12:20:01 +0200

    test if all cars are returned

--- backend/tests/carApi.test.js
+++ backend/tests/carApi.test.js
@@ -2,13 +2,24 @@ const mongoose = require('mongoose')
 const helper = require('./testHelper')
 const supertest = require('supertest')
 const app = require('../app')
-
+const Car = require('../models/car')
+const mockData = require('../utils/_data')
 const api = supertest(app)
+
+beforeEach(async () => {
+    await helper.populateCarModels()
+    await helper.populateCars()
+})
 
 test('cars are returned as json', async () => {
     await api.get('/api/cars')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+})
+
+test('all cars are returned', async () => {
+    const response = await api.get('/api/cars')
+    expect(response.body).toHaveLength(mockData.carModels.length)
 })
 
 afterAll(() => {
