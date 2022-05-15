f075f2e69cf6bd07e7272c92bd96a394fd9ce682
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-05-15 12:18:30 +0200

    add additional helper functions

--- backend/tests/testHelper.js
+++ backend/tests/testHelper.js
@@ -1,9 +1,19 @@
 const CarModel = require('../models/carmodel')
 const Car = require('../models/car')
+const {
+    carModels
+} = require('../utils/_data')
 
 const carModelsInDb = async () => {
     const carModels = await CarModel.find({})
     return carModels
+}
+
+const populateCarModels = async () => {
+    await CarModel.deleteMany({})
+    const objects = carModels.map(carModel => new CarModel(carModel))
+    const promiseArray = objects.map(object => object.save())
+    await Promise.all(promiseArray)
 }
 
 const carsInDb = async () => {
@@ -11,7 +21,16 @@ const carsInDb = async () => {
     return cars
 }
 
+const populateCars = async () => {
+    await Car.deleteMany({})
+    const carModels = await carModelsInDb()
+    const carModelIds = carModels.map(carModel => carModel._id)
+    const objects = carModelIds.map(carModelId => new Car({ carModel: carModelId }))
+    const promiseArray = objects.map(object => object.save())
+    await Promise.all(promiseArray)
+}
+
 module.exports = {
-    carModelsInDb,
-    carsInDb
+    carModelsInDb, populateCarModels,
+    carsInDb, populateCars
 }
