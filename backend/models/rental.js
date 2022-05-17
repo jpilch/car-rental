af3970dd8e0ba65e96ba746b59979487b061d7fb
jpilch <pilch.jan.kontakt@gmail.com>
2022-05-17 14:45:30 +0200

    create rental model

---
+++ backend/models/rental.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const rentalSchema = new mongoose.Schema({
+    city_en: {
+        required: true,
+        type: String
+    },
+    city_pl: {
+        required: true,
+        type: String
+    },
+    address: {
+        required: true,
+        type: String
+    },
+    cars: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Car'
+        }
+    ]
+})
+
+rentalSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Rental', rentalSchema)
