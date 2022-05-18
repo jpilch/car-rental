947d1ab25fe36994c698a631ae5d97bdca6b5bf8
jpilch <pilch.jan.kontakt@gmail.com>
2022-05-18 17:48:33 +0200

    add script for populating database

---
+++ backend/mongo.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const helper = require('./utils/helper')
+
+const populate = async () => {
+    await helper.populateCarModels()
+}
+
+mongoose.connect(`${config.MONGODB_URI}`)
+    .then(() => {
+        logger.info('Connected to MongoDB')
+        populate()
+            .then(() => {
+                logger.info('Populating database succeeded')
+                mongoose.connection.close()
+            })
+            .catch(err => logger.error(err))
+    })
+    .catch(err => {
+        logger.info('MongoDB connection fail')
+        logger.error(err)
+    })
