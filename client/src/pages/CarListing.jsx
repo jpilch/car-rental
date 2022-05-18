d61551358039276e6d557083deca7b614b30bf3d
jpilch <pilch.jan.kontakt@gmail.com>
2022-05-18 18:31:22 +0200

    create car listing page layout

--- client/src/pages/CarListing.jsx
+++ client/src/pages/CarListing.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import '../css/CarListing.css'
 
 function CarListing() {
     return (
         <main id="car-list">
-            <h1>Car list</h1>
+            <h1>Check out our amazing offer</h1>
+            <div className="sort-by">
+                <label htmlFor="sort">Sort by: </label>
+                <select name="sort" id="sort">
+                    <option value="price-desc">price descending</option>
+                    <option value="price-asc">price ascending</option>
+                    <option value="popularity">popularity</option>
+                </select>
+            </div>
+            <section className='cars'>
+                <aside className='filters'>
+                    <p>filters</p>
+                </aside>
+                <div>
+                    <p>cars</p>
+                </div>
+            </section>
         </main>
     )
 }
