c73c6c0b1888f1a385714e716d18928415dc2c1e
jpilch <pilch.jan.kontakt@gmail.com>
2022-05-18 07:52:37 +0200

    add beforeAll and beforeEach method to rental tests

--- backend/tests/rentalApi.test.js
+++ backend/tests/rentalApi.test.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose')
 const app = require('../app')
 const supertest = require('supertest')
+const Rental = require('../models/rental')
+const helper = require("./testHelper")
 
 const api = supertest(app)
 
+beforeAll(async () => {
+    await helper.clearAll()
+})
+
+beforeEach(async () => {
+    await Rental.deleteMany({})
+    await helper.populateRentals()
+})
+
 test('rentals are returned as json', async () => {
-    await api.get('/api/rentals')
+    const response = await api.get('/api/rentals')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength(2)
 })
 
 afterAll(() => {
