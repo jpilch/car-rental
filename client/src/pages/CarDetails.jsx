1c408dde8d420633dfd318bd84e8922e8c96436b
jpilch <pilch.jan.kontakt@gmail.com>
2022-05-19 13:27:21 +0200

    fetch car in car details page

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,12 +1,22 @@
 import '../css/CarDetails.css'
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const CarDetails = () => {
     const { id } = useParams()
+    const [car, setCar] = useState({})
+    useEffect(async () => {
+        const response = await axios.get(
+            `${process.env.REACT_APP_API_URL}/car-models/${id}`
+        )
+        setCar(response.data)
+    }, [])
 
     return (
         <main id="car-details">
             <h1>{id}</h1>
+            <h1>{car.manufacturer} {car.name}</h1>
         </main>
     )
 }
