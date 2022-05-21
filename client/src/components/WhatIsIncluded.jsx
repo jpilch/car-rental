e720873e7df9bcb271b68eb353e28b8e6f738f34
jpilch <pilch.jan.kontakt@gmail.com>
2022-05-21 17:08:32 +0200

    add more boilerplace benefits

--- client/src/components/WhatIsIncluded.jsx
+++ client/src/components/WhatIsIncluded.jsx
@@ -8,12 +8,16 @@ const WhatIsIncluded = () => {
             <h2>Great choice!</h2>
             <div className="benefits">
                 <ul>
-                    <OfferBenefit text={'No additional fees!'}/>
-                    <OfferBenefit text={'Free to cancel anytime!'}/>
+                    <OfferBenefit text={'No additional fees'}/>
+                    <OfferBenefit text={'Free to cancel anytime'}/>
                 </ul>
                 <ul>
                     <OfferBenefit text={'Short to no queues'}/>
                     <OfferBenefit text={'Dedicated parking space'}/>
+                </ul>
+                <ul>
+                    <OfferBenefit text={'Hundreds of satisfied customers'}/>
+                    <OfferBenefit text={'Low prices and high quality service'}/>
                 </ul>
             </div>
         </section>
