0efecf9254c0aee6967aa077dea34dc5856c7d18
jpilch <pilch.jan.kontakt@gmail.com>
2022-05-26 09:27:37 +0200

    prevent user from renting multiple cars simultaneously

--- client/src/components/OfferActions.jsx
+++ client/src/components/OfferActions.jsx
@@ -5,26 +5,51 @@ import {useNavigate} from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import {useSelector, useDispatch} from "react-redux";
 import {createAgreement} from "../reducers/offerSlice";
+import Modal from "./Modal";
+import {toggleModal} from "../reducers/modalSlice";
+import useUserInfo from "../hooks/useUserInfo";
+import Loading from "./Loading";
 
 const OfferActions = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const {token} = useAuth()
+    const { userInfo } = useUserInfo()
+    const token = useAuth()
     const data = useSelector(state => state.offerReducer)
 
+    if (!userInfo) {
+        return <Loading />
+    }
+
+    const userCanRent = () => {
+        for (let agreement of userInfo.agreements) {
+            if (agreement.active || agreement.planned) {
+                return false
+            }
+        }
+        return true
+    }
+
+    console.log(userCanRent())
+
     return (
-        <div className="actions">
-            <SimpleButton
-                bgColor={'var(--clr-red)'}
-                text={'Cancel'}
-                onClick={() => navigate('/')}
+        <>
+            <Modal
+                text={'You currently have a rented car. More details at your account\'s agreement page.'}
+                onConfirm={() => console.log('hello')}
             />
-            <SimpleButton
-                text={'Pay'}
-                bgColor={'var(--clr-green)'}
-                onClick={() => dispatch(createAgreement(data, token, navigate))}
-            />
-        </div>
+            <div className="actions">
+                <SimpleButton
+                    text={'Pay'}
+                    bgColor={'var(--clr-green)'}
+                    onClick={userCanRent()
+                        ? () => dispatch(createAgreement(data, token, navigate))
+                        : () => dispatch(toggleModal())
+                    }
+                />
+            </div>
+        </>
+
     )
 }
 
